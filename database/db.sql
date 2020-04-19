aade73457a79159a2f07932ab618e35ce4ebdf6e
André Oliveira <inforoliver@gmail.com>
2020-04-19 15:27:41 -0300

    Database Ok

--- database/db.sql
+++ database/db.sql
@@ -2,6 +2,7 @@ create database database_links;
 
 use database_links;
 
+-- user table
 create table users(
     id int(11) not null,
     username varchar(16) default '' not null,
@@ -15,4 +16,22 @@ alter table users
 alter table users
     modify id int(11) not null auto_increment, auto_increment = 2;
 
-describe users;
+--describe users;
+
+-- links tables
+create table links (
+    id int(11) not null,
+    title varchar(150) default '' not null,
+    url varchar(255) default '' not null,
+    description text,
+    user_id int(11),
+    created_at timestamp default current_timestamp not null,
+
+    constraint fk_user foreign key (user_id) references users(id)
+);
+
+alter table links
+    add primary key (id);
+
+alter table links
+    modify id int(11) not null auto_increment, auto_increment = 2;
---
+++ src/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    database: {
+        host: 'localhost',
+        user: 'adm',
+        password: '123456',
+        database: 'databae_links'
+    }
+};
