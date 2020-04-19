fd900862393645fde8ccd27cc18b7f45ff5896d8
André Oliveira <inforoliver@gmail.com>
2020-04-19 15:11:02 -0300

    Add database

---
+++ database/db.sql
@@ -0,0 +1,18 @@
+create database database_links;
+
+use database_links;
+
+create table users(
+    id int(11) not null,
+    username varchar(16) default '' not null,
+    password varchar(60) default '' not null,
+    fullname varchar(100) default '' not null
+);
+
+alter table users
+    add primary key (id);
+
+alter table users
+    modify id int(11) not null auto_increment, auto_increment = 2;
+
+describe users;
